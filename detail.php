21683c15ca31e4aad1c10bc5d130dd388a5c6afe
NguyenDinhThang1204 <2051063572@e.tlu.edu.vn>
2023-02-25 22:27:55 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,23 +1,34 @@
 <?php include "./includes/header.php" ?>
-    </header>
-    <main class="container mt-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
-       
-                <div class="row mb-5">
-                    <div class="col-sm-4">
-                        <img src="images/songs/cayvagio.jpg" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col-sm-8">
-                        <h5 class="card-title mb-2">
-                            <a href="" class="text-decoration-none">Cây và gió</a>
-                        </h5>
-                        <p class="card-text"><span class=" fw-bold">Bài hát: </span>Cây và gió</p>
-                        <p class="card-text"><span class=" fw-bold">Thể loại: </span>Nhạc trữ tình</p>
-                        <p class="card-text"><span class=" fw-bold">Tóm tắt: </span>Em và anh, hai đứa quen nhau thật tình cờ. Lời hát của anh từ bài hát “Cây và gió” đã làm tâm hồn em xao động. Nhưng sự thật phũ phàng rằng em chưa bao giờ nói cho anh biết những suy nghĩ tận sâu trong tim mình. Bởi vì em nhút nhát, em không dám đối mặt với thực tế khắc nghiệt, hay thực ra em không dám đối diện với chính mình.</p>
-                        <p class="card-text"><span class=" fw-bold">Nội dung: </span>Em và anh, hai đứa quen nhau thật tình cờ. Lời hát của anh từ bài hát “Cây và gió” đã làm tâm hồn em xao động. Nhưng sự thật phũ phàng rằng em chưa bao giờ nói cho anh biết những suy nghĩ tận sâu trong tim mình. Bởi vì em nhút nhát, em không dám đối mặt với thực tế khắc nghiệt, hay thực ra em không dám đối diện với chính mình.</p>
-                        <p class="card-text"><span class=" fw-bold">Tác giả: </span>Nguyễn Văn Giả</p>
+</header>
+<?php
+    require './includes/database-connection.php';
+    require './includes/functions.php';
+    require './includes/inc_change_slug.php';
+    $id = $_GET['id'];
+    $sql = "SELECT baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat,baiviet.tomtat, tacgia.ten_tgia, theloai.ten_tloai, baiviet.ngayviet, baiviet.hinhanh FROM baiviet JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai WHERE baiviet.ma_bviet = '" . $id . "'";
+    $result = pdo($pdo, $sql)->fetchAll();
+    ?>
 
-                    </div>          
+<main class="container mt-5">
+    <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+
+    <div class="row mb-5">
+        <div class="col-sm-4">
+            <img src="images/songs/<?php echo !empty($result[0]['hinhanh']) ? $result[0]['hinhanh'] : (str_replace('-','',create_slug($result[0]['ten_bhat'])).".jpg")  ?>"
+                class="img-fluid" alt="...">
+
         </div>
-    </main>
+        <div class="col-sm-8">
+            <h5 class="card-title mb-2">
+                <a href="" class="text-decoration-none"><?php echo $result[0]['ten_bhat'] ?></a>
+            </h5>
+            <p class="card-text"><span class=" fw-bold">Bài hát: </span><?php echo $result[0]['ten_bhat'] ?></p>
+            <p class="card-text"><span class=" fw-bold">Thể loại: </span><?php echo $result[0]['ten_tloai']?></p>
+            <p class="card-text"><span class=" fw-bold">Tóm tắt: </span><?php echo $result[0]['tomtat'] ?></p>
+            <p class="card-text"><span class=" fw-bold">Nội dung: </span><?php echo $result[0]['tomtat'] ?></p>
+            <p class="card-text"><span class=" fw-bold">Tác giả: </span><?php echo $result[0]['ten_tgia'] ?></p>
+
+        </div>
+    </div>
+</main>
 <?php include "./includes/footer.php" ?>
