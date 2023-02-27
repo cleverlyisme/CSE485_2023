b42333a53b0e0f6a91af574d36924a50e05053be
NguyenDinhThang1204 <2051063572@e.tlu.edu.vn>
2023-02-27 22:53:26 +0700

    fix: CSS admin/home/index.php

--- admin/home/index.php
+++ admin/home/index.php
@@ -17,60 +17,68 @@
     <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
     <div class="row">
         <div class="col-sm-3">
-            <div class="card mb-2" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title text-center">
-                        <a href="" class="text-decoration-none">Người dùng</a>
-                    </h5>
+            <div class="card-box">
+                <div class="mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Người dùng</a>
+                        </h5>
 
-                    <h5 class="h1 text-center">
-                        <?php echo count($countUsers) ?>
-                    </h5>
+                        <h5 class="h1 text-center">
+                            <?php echo count($countUsers) ?>
+                        </h5>
+                    </div>
                 </div>
             </div>
         </div>
 
         <div class="col-sm-3">
-            <div class="card mb-2" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title text-center">
-                        <a href="" class="text-decoration-none">Thể loại</a>
-                    </h5>
-                    <h5 class="h1 text-center">
-                        <?php echo count($countTheloai) ?>
-                    </h5>
+            <div class="card-box">
+                <div class=" mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Thể loại</a>
+                        </h5>
+                        <h5 class="h1 text-center">
+                            <?php echo count($countTheloai) ?>
+                        </h5>
+                    </div>
                 </div>
             </div>
         </div>
 
         <div class="col-sm-3">
-            <div class="card mb-2" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title text-center">
-                        <a href="" class="text-decoration-none">Tác giả</a>
-                    </h5>
+            <div class="card-box">
+                <div class="mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Tác giả</a>
+                        </h5>
 
-                    <h5 class="h1 text-center">
-                        <?php echo count($countTacgia) ?>
-
-                    </h5>
+                        <h5 class="h1 text-center">
+                            <?php echo count($countTacgia) ?>
+                        </h5>
+                    </div>
                 </div>
             </div>
+
         </div>
 
         <div class="col-sm-3">
-            <div class="card mb-2" style="width: 100%;">
-                <div class="card-body">
-                    <h5 class="card-title text-center">
-                        <a href="" class="text-decoration-none">Bài viết</a>
-                    </h5>
+            <div class="card-box">
+                <div class="mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Bài viết</a>
+                        </h5>
 
-                    <h5 class="h1 text-center">
-                        <?php echo count($countBaiViet) ?>
-
-                    </h5>
+                        <h5 class="h1 text-center">
+                            <?php echo count($countBaiViet) ?>
+                        </h5>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 </main>
